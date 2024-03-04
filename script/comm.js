a0f87d885c3a8983954104419dd2ef6166f11f7c
han0won <han00won@gmail.com>
2024-03-04 13:18:06 +0900

    .

--- script/comm.js
+++ script/comm.js
@@ -1,14 +1,4 @@
 $(function () {
-    // let now = 0;
-
-    // const slideCount = ('.slide ul li')
-    // const slideList = ('.slide ul')
-    
-    // setInterval(function () {
-    //     now = (now + 1) % slideCount
-    //     slideList.css('left', 100 * -now + '%')
-    // }, 3000)
-
     $('.notice ul li:first-child').on('click', function () {
         $('#modal').css('display', 'flex')
     })
